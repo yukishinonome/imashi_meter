ea0d19a51c6e6a80f39cbe3e976a8b7217c9596f
YukiShinonome <yuki.yamakoshi0126@gmail.com>
2020-05-22 15:17:44 +0900

    Eslintの設定変更

--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,18 @@
 <template>
   <v-layout column justify-center align-center>
     <v-flex xs12 sm8 md6>
-      <v-card>
-        <v-card-title class="headline">Welcome to the Vuetify + Nuxt.js template</v-card-title>
-        <v-card-text>
-          <p>
-            Vuetify is a progressive Material Design component framework for
-            Vue.js. It was designed to empower developers to create amazing
-            applications.
-          </p>
-          <hr class="my-3" />
-          <a href="https://nuxtjs.org/" target="_blank">Nuxt Documentation</a>
-          <br />
-          <a href="https://github.com/nuxt/nuxt.js" target="_blank">Nuxt GitHub</a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn color="primary" nuxt to="/inspire">Continue</v-btn>
-        </v-card-actions>
+      <v-card class="mx-auto" max-width="800" outlined>
+        <v-list-item three-line>
+          <v-list-item-content>
+            <div class="overline mb-4">OVERLINE</div>
+            <v-list-item-title class="headline mb-1">
+              課金しているイラスト
+            </v-list-item-title>
+            <v-list-item-subtitle>
+              Greyhound divisely hello coldly fonwderfully
+            </v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
       </v-card>
     </v-flex>
   </v-layout>
