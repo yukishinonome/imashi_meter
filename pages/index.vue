26590eec22dd35cc58fcab3b47f5ec4abcc97c41
YukiShinonome <yuki.yamakoshi0126@gmail.com>
2020-05-22 15:27:44 +0900

    components/HogeCard.vueの作成

--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,16 @@
 <template>
   <v-layout column justify-center align-center>
     <v-flex xs12 sm8 md6>
-      <v-card class="mx-auto" max-width="800" outlined>
-        <v-list-item three-line>
-          <v-list-item-content>
-            <div class="overline mb-4">OVERLINE</div>
-            <v-list-item-title class="headline mb-1">
-              課金しているイラスト
-            </v-list-item-title>
-            <v-list-item-subtitle>
-              Greyhound divisely hello coldly fonwderfully
-            </v-list-item-subtitle>
-          </v-list-item-content>
-        </v-list-item>
-      </v-card>
+      <hoge-card />
     </v-flex>
   </v-layout>
 </template>
 
 <script>
-export default {}
+import HogeCard from '~/components/HogeCard.vue'
+export default {
+  components: {
+    HogeCard
+  }
+}
 </script>
