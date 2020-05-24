d0a834021f244d2d650418567203263498f6fba2
YukiShinonome <yuki.yamakoshi0126@gmail.com>
2020-05-24 21:57:06 +0900

    components/HogeCard&Imashime: v-cardの変更

--- components/HogeCard.vue
+++ components/HogeCard.vue
@@ -3,9 +3,8 @@
     <v-card class="mx-auto" max-width="800" outlined>
       <v-list-item three-line>
         <v-list-item-content>
-          <div class="overline mb-4">OVERLINE</div>
+          <v-list-item-avatar tile size="80" color="grey"></v-list-item-avatar>
           <v-list-item-title class="headline mb-1">課金しているイラスト</v-list-item-title>
-          <v-list-item-subtitle>Greyhound divisely hello coldly fonwderfully</v-list-item-subtitle>
         </v-list-item-content>
       </v-list-item>
     </v-card>
--- components/ImashimeCard.vue
+++ components/ImashimeCard.vue
@@ -3,10 +3,12 @@
     <v-card class="mx-auto" max-width="800" outlined>
       <v-list-item three-line>
         <v-list-item-content>
-          <div class="overline mb-4">戒め</div>
           <v-list-item-avatar tile size="80" color="grey"></v-list-item-avatar>
-          <v-list-item-title class="headline mb-1">課金しているイラスト</v-list-item-title>
-          <v-list-item-subtitle>Greyhound divisely hello coldly fonwderfully</v-list-item-subtitle>
+          <v-list-item-title class="headline mb-1">戒めリストのイラスト</v-list-item-title>
+          <v-list-item-subtitle>
+            あなたが今投じた金額は…
+            <br />うな重10杯分です！！
+          </v-list-item-subtitle>
         </v-list-item-content>
       </v-list-item>
     </v-card>
