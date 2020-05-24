3267cf2cfed529ae6073b1cf5cdd528eab22c67c
YukiShinonome <yuki.yamakoshi0126@gmail.com>
2020-05-24 22:43:58 +0900

    components/InputScreen: v-text-fieldにtype="number"を追加

--- components/InputScreen.vue
+++ components/InputScreen.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
     <div>カテゴリー</div>
-    <div>課金額</div>
-    <v-text-field label="懺悔せよ" single-line outlined></v-text-field>
+    <div>課金額（半角数字）</div>
+    <v-text-field label="懺悔せよ" suffix="円" single-line outlined type="number"></v-text-field>
   </div>
 </template>
 
