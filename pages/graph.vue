5a5e29ccb8d10e4ef8cd2ebb63073c19789a155d
yoshikawa <kosuke-yoshikawa@gmo-k.jp>
2020-05-26 20:50:18 +0900

    pages/graph: グラフ下のリスト追加

--- pages/graph.vue
+++ pages/graph.vue
@@ -7,38 +7,42 @@
         </v-list-item-content>
       </v-list-item>
     </v-card>
-    <v-list-item>
-      <v-list-item-content>
-        <v-list-item-title>Single-line item</v-list-item-title>
-      </v-list-item-content>
-    </v-list-item>
     <v-divider></v-divider>
-    <v-list-item>
-      <v-list-item-content>
-        <v-list-item-title>Single-line item</v-list-item-title>
-      </v-list-item-content>
-    </v-list-item>
-    <v-divider></v-divider>
-    <v-list-item>
-      <v-list-item-content>
-        <v-list-item-title>Single-line item</v-list-item-title>
-      </v-list-item-content>
-    </v-list-item>
-    <v-divider></v-divider>
-    <v-list-item>
-      <v-list-item-content>
-        <v-list-item-title>Single-line item</v-list-item-title>
-      </v-list-item-content>
-    </v-list-item>
-    <v-divider></v-divider>
-    <v-list-item>
-      <v-list-item-content>
-        <v-list-item-title>Single-line item</v-list-item-title>
-      </v-list-item-content>
-    </v-list-item>
+    <v-simple-table fixed-header dark height="300px">
+      <thead>
+        <tr class="text-left">
+          <th class="font-weight-black">総額</th>
+          <th class="font-weight-black">{{totalAmount}}</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="history in histories" :key="history.id">
+          <td>{{history.category}}</td>
+          <td>{{history.amounts}}</td>
+        </tr>
+      </tbody>
+    </v-simple-table>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data(){
+    return {
+      totalAmount: String(1888) + "円",
+      histories:[]
+    }
+  },
+
+  methods: {
+    // APIより履歴データをロード
+    async loadHistories() {
+      const data = await this.$axios.$get('https://api-server-gtb.herokuapp.com/histories')
+      this.histories = data;
+    }
+  },
+  mounted() {
+    this.loadHistories()
+  }
+}
 </script>
