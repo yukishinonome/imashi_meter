e03c9a69d045e38e9f55e9e00d2b405ff9d35c38
yoshikawa <kosuke-yoshikawa@gmo-k.jp>
2020-05-26 15:50:39 +0900

    表のカラムの順番を変更

--- pages/history.vue
+++ pages/history.vue
@@ -1,25 +1,26 @@
 <template>
   <div>
-    <v-list-item-title>履歴</v-list-item-title>
-    <v-simple-table>
+    <br>
+    <h2>履歴</h2>
+    <br>
+    <v-divider></v-divider>
+    <v-simple-table fixed-header>
       <thead>
         <tr>
-          <th class="text-left">ID</th>
-          <th class="text-left">課金額</th>
           <th class="text-left">カテゴリー</th>
+          <th class="text-left">課金額</th>
           <th class="text-left">日付</th>
         </tr>
       </thead>
       <tbody>
         <tr v-for="history in histories" :key="history.id">
-          <td>{{history.id}}</td>
-          <td>{{history.amounts}}</td>
           <td>{{history.category}}</td>
+          <td>{{history.amounts}}</td>
           <td>{{history.created_at.slice(0, 10)}}</td>
         </tr>
       </tbody>
     </v-simple-table>
-    <!-- <v-list-item v-for="n of 5" :key="n">
+    <!-- <v-list-item v-for="history in histories">
       <v-list-item-content>
         <v-list-item-title>{{history.amounts}}</v-list-item-title>
         <v-list-item-title>{{history.category}}</v-list-item-title>
@@ -33,21 +34,7 @@
 export default {
   data(){
     return {
-      histories:[],
-      items: [
-        {
-          id: 1,
-          amounts: 1230,
-          category: "カジノ",
-          created_at: "2020/06/23"
-        },
-        {
-          id: 2,
-          amounts: 1230,
-          category: "カジノ",
-          created_at: "2020/06/23"
-        }
-      ]
+      histories:[]
     }
   },
   methods: {
