cb6b421bfe8eea8e7a2f436c21ff270abdb0924b
YukiShinonome <yuki.yamakoshi0126@gmail.com>
2020-05-27 11:59:37 +0900

    pages/index: 見た目を整える

--- pages/index.vue
+++ pages/index.vue
@@ -1,36 +1,36 @@
 <template>
   <div>
-    <div v-if="printPage == 'MAIN'" class="d-flex flex-column align-center">
-      <hoge-card />
-      <v-btn color="orange" @click="toInput()">課金金額入力</v-btn>
+    <div
+      v-if="printPage == 'MAIN'"
+      class="d-flex flex-column justify-space-around align-center container"
+    >
       <div>
-        今月の課金総額
-        <br />＊＊＊円
+        <div class="circle d-flex flex-column justify-center align-center">
+          <div>今月の課金総額</div>
+          <div>
+            <span class="big-text">＊＊＊</span>円
+          </div>
+        </div>
       </div>
-      <div>
-        課金総額
-        <br />＊＊＊円
-      </div>
+      <v-btn color="#006FFF" outlined rounded @click="toInput()">課金金額入力</v-btn>
     </div>
     <div v-else-if="printPage == 'INPUT'" class="d-flex flex-column align-center">
       <input-screen />
-      <v-btn color="orange" @click="toImashime()">追加</v-btn>
+      <v-btn color="#006FFF" outlined rounded @click="toImashime()">追加</v-btn>
     </div>
     <div v-else class="d-flex flex-column align-center">
       <imashime-card />
-      <v-btn color="orange" @click="toMain()">閉じる</v-btn>
+      <v-btn color="#006FFF" outlined rounded @click="toMain()">閉じる</v-btn>
     </div>
   </div>
 </template>
 
 <script>
-import HogeCard from '~/components/HogeCard.vue'
 import InputScreen from '~/components/InputScreen.vue'
 import ImashimeCard from '~/components/ImashimeCard.vue'
 
 export default {
   components: {
-    HogeCard,
     InputScreen,
     ImashimeCard
   },
@@ -50,3 +50,33 @@ export default {
   }
 }
 </script>
+
+<style lang="scss">
+.container {
+  height: 80vh;
+}
+
+.circle {
+  border: 12px solid #006fff;
+  width: 300px;
+  height: 300px;
+  border-radius: 50%;
+  font-size: 1.2rem;
+}
+
+.big-text {
+  font-size: 2.2rem;
+}
+
+.v-btn.v-size--default {
+  font-size: 1.2rem;
+}
+
+.v-btn:not(.v-btn--round).v-size--default {
+  height: 40px;
+}
+
+.v-btn--outlined {
+  border: 2px solid #006fff;
+}
+</style>
