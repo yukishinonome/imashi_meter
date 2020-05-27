1052a4675d1e453f8a4ba187dcf5c32f8c527f16
yoshikawa <kosuke-yoshikawa@gmo-k.jp>
2020-05-27 21:01:07 +0900

    pages/graph: 今月の課金総額計算機能追加

--- components/graphScreen.vue
+++ components/graphScreen.vue
@@ -3,78 +3,20 @@ import { Doughnut } from 'vue-chartjs'
 
 export default {
   extends: Doughnut,
-  data() {
-    return {
-      histories: [],
-      thisMonthHistories: [],
-      datas: {
-        labels: [] /* ['ゲーム', '酒'] */,
-        datasets: [
-          {
-            data: [] /* [45000, 15000] */,
-            backgroundColor: [] /* ['#006FFF', '#F2C94C'] */
-          }
-        ]
-      },
-      options: {
-        responsive: true
-      }
+  props: {
+    chartdata: {
+      type: Object,
+      default: null
+    },
+    options: {
+      type: Object,
+      default: null,
+      responsive: true
     }
   },
   mounted() {
-    this.loadHistory()
-    // this.loadGraphData();
-    this.renderChart(this.datas, this.options)
+    this.renderChart(this.chartdata, this.options)
   },
-  methods: {
-    getHistoryMonth(history) {
-      const month = history.created_at.slice(5, 7)
-      let historyMonth
-
-      if (month.match(/0/)) {
-        // もし1（01）〜9（09）月なら
-        historyMonth = month.substring(1, 2)
-      } else {
-        // もし10（10）〜12（12）月なら
-        historyMonth = month
-      }
-      return historyMonth
-    },
-
-    // APIより履歴データをロード
-    async loadHistory() {
-      const historyData = await this.$axios.$get(
-        'https://api-server-gtb.herokuapp.com/histories'
-      )
-
-      const now = new Date()
-      const thisMonth = now.getMonth() + 1
-
-      // 今月である履歴データだけ取り出す
-      for (let i = 0; i < historyData.length; i++) {
-        if (this.getHistoryMonth(historyData[i]) == thisMonth) {
-          console.log('今月は等しい')
-          // 今月分の履歴のみthisMonthHistoriesに追加
-          this.thisMonthHistories.push(historyData[i])
-        } else {
-          console.log('今月は等しくない')
-        }
-      }
-      console.log(this.thisMonthHistories)
-
-      // 重複がないようにカテゴリーを配列labelsに追加
-      for (let i = 0; i < this.thisMonthHistories.length; i++) {
-        this.datas.labels.push(this.thisMonthHistories[i].category)
-        this.datas.datasets[0].data.push(this.thisMonthHistories[i].amounts)
-        this.datas.datasets[0].backgroundColor.push('#006FFF')
-      }
-      // for (const [key, value] of Object.entries(this.thisMonthHistories)) {
-      //   this.labels.push(value.category)
-      // }
-      console.log(this.datas.labels)
-      console.log(this.datas.datasets[0].data)
-      console.log(this.datas.datasets[0].backgroundColor)
-    }
-  }
+  methods: {}
 }
 </script>
--- pages/graph.vue
+++ pages/graph.vue
@@ -1,19 +1,20 @@
 <template>
   <div>
     <div class="text-center">今月</div>
-    <doughnut-chart-component></doughnut-chart-component>
+    <!-- <doughnut-chart-component v-if="loaded" :chartdata="datas" :options="options" /> -->
+    <canvas id="myChart"></canvas>
     <v-divider></v-divider>
     <v-simple-table fixed-header dark height="300px">
       <thead>
         <tr class="text-left">
           <th>総額</th>
-          <th>{{totalAmount}}</th>
+          <th>{{ totalAmount }}</th>
         </tr>
       </thead>
       <tbody>
         <tr v-for="history in histories" :key="history.id">
-          <td>{{history.category}}</td>
-          <td>{{history.amounts}}</td>
+          <td>{{ history.category }}</td>
+          <td>{{ history.amounts }}</td>
         </tr>
       </tbody>
     </v-simple-table>
@@ -28,21 +29,93 @@ export default {
   components: {
     graphScreen
   },
-  data(){
+  data() {
     return {
-      totalAmount: String(1888) + "円",
-      histories:[]
-    }
-  },
-  methods: {
-    // APIより履歴データをロード
-    async loadHistories() {
-      const data = await this.$axios.$get('https://api-server-gtb.herokuapp.com/histories')
-      this.histories = data;
+      loaded: false,
+      options: {
+        responsive: true
+      },
+      data: {
+        labels: [],
+        data: []
+      },
+      totalAmount: 0,
+      histories: [],
+      thisMonthHistories: []
     }
   },
   mounted() {
     this.loadHistories()
+    // this.renderChart(this.datas, this.options)
+  },
+  methods: {
+    // 各履歴の月を取得
+    getHistoryMonth(history) {
+      const month = history.created_at.slice(5, 7)
+      let historyMonth
+
+      if (month.match(/0/)) {
+        // もし1（01）〜9（09）月なら
+        historyMonth = month.substring(1, 2)
+      } else {
+        // もし10（10）〜12（12）月なら
+        historyMonth = month
+      }
+      return historyMonth
+    },
+
+    // 各カテゴリーの総額計算
+    calculateCategoriesAmounts(histories) {},
+
+    calculateTotalAmounts(histories) {
+      Object.entries(histories).forEach(([key, value]) => {
+        console.log(value.amounts)
+        this.totalAmount += value.amounts
+      })
+    },
+
+    // APIより履歴データをロード
+    async loadHistories() {
+      this.loaded = false
+      try {
+        const historyData = await this.$axios.$get(
+          'https://api-server-gtb.herokuapp.com/histories'
+        )
+
+        const now = new Date()
+        const thisMonth = now.getMonth() + 1
+
+        // 今月である履歴データだけ取り出す
+        for (let i = 0; i < historyData.length; i++) {
+          if (this.getHistoryMonth(historyData[i]) == thisMonth) {
+            console.log('今月は等しい')
+            // 今月分の履歴のみthisMonthHistoriesに追加
+            this.thisMonthHistories.push(historyData[i])
+          } else {
+            console.log('今月は等しくない')
+          }
+        }
+        console.log(this.thisMonthHistories)
+        this.calculateTotalAmounts(this.thisMonthHistories)
+
+        // 重複がないようにカテゴリーを配列labelsに追加
+        for (let i = 0; i < this.thisMonthHistories.length; i++) {
+          // this.datas.labels.push(this.thisMonthHistories[i].category)
+          // this.datas.datasets[0].data.push(this.thisMonthHistories[i].amounts)
+          // this.datas.datasets[0].backgroundColor.push('#006FFF')
+        }
+
+        this.histories = historyData
+
+        console.log(this.datas.labels)
+        console.log(this.datas.datasets[0].data)
+        console.log(this.datas.datasets[0].backgroundColor)
+
+        this.loaded = true
+      } catch (e) {
+        console.error(e)
+      }
+    }
   }
 }
 
