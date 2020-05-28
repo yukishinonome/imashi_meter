7c98e376501b03ac9b06fbc30537a59ed80ac935
yoshikawa <kosuke-yoshikawa@gmo-k.jp>
2020-05-28 15:08:33 +0900

    pages/graph: 関数を整理

--- pages/graph.vue
+++ pages/graph.vue
@@ -53,17 +53,9 @@ export default {
   methods: {
     // 各履歴の月を取得
     getHistoryMonth(history) {
-      const month = history.created_at.slice(5, 7)
-      let historyMonth
-
-      if (month.match(/0/)) {
-        // もし1（01）〜9（09）月なら
-        historyMonth = month.substring(1, 2)
-      } else {
-        // もし10（10）〜12（12）月なら
-        historyMonth = month
-      }
-      return historyMonth
+      let month = history.created_at.slice(5, 7)
+      month = parseInt(month)
+      return month
     },
 
     // 各カテゴリーの総額計算
@@ -82,7 +74,7 @@ export default {
       Object.entries(categoryArray).forEach(([categoryKey, categoryValue]) => {
         sum[categoryKey] = 0
         Object.entries(histories).forEach(([historyKey, history]) => {
-          if (categoryValue == history.category) {
+          if (categoryValue === history.category) {
             sum[categoryKey] = sum[categoryKey] + history.amounts
             map.set(categoryValue, sum[categoryKey])
           }
@@ -124,10 +116,7 @@ export default {
 
         Object.entries(historyData).forEach(([key, value]) => {
           if (this.getHistoryMonth(value) == thisMonth) {
-            console.log('今月に等しい')
             this.thisMonthHistories.push(value)
-          } else {
-            console.log('今月ではない')
           }
         })
 
@@ -135,21 +124,21 @@ export default {
         this.calculateTotalAmounts(this.thisMonthHistories)
         this.calculateCategoriesAmounts(this.thisMonthHistories)
 
-        // 重複がないようにカテゴリーを配列labelsに追加
-        for (let i = 0; i < this.thisMonthHistories.length; i++) {
-          // this.datas.labels.push(this.thisMonthHistories[i].category)
-          // this.datas.datasets[0].data.push(this.thisMonthHistories[i].amounts)
-          // this.datas.datasets[0].backgroundColor.push('#006FFF')
-        }
+        // // 重複がないようにカテゴリーを配列labelsに追加
+        // for (let i = 0; i < this.thisMonthHistories.length; i++) {
+        //   this.datas.labels.push(this.thisMonthHistories[i].category)
+        //   this.datas.datasets[0].data.push(this.thisMonthHistories[i].amounts)
+        //   this.datas.datasets[0].backgroundColor.push('#006FFF')
+        // }
 
-        this.histories = historyData
+        // this.histories = historyData
 
-        this.fillData(2, 3, 4)
+        // this.fillData(2, 3, 4)
 
         // console.log(this.datas.labels)
         // console.log(this.datas.datasets[0].data)
         // console.log(this.datas.datasets[0].backgroundColor)
-
+        console.log(this.categoriesAmounts)
         this.loaded = true
       } catch (e) {
         console.error(e)
