4f6373ca31652f4a730811e4ce62f814f985d964
yoshikawa <kosuke-yoshikawa@gmo-k.jp>
2020-05-29 02:31:37 +0900

    components/ImashimeCard: 課金額に対してランダムに画像表示

--- components/ImashimeCard.vue
+++ components/ImashimeCard.vue
@@ -1,20 +1,74 @@
 <template>
   <div>
-    <v-card class="mx-auto" max-width="800" outlined>
-      <v-list-item three-line>
-        <v-list-item-content>
-          <v-list-item-avatar tile size="80" color="grey"></v-list-item-avatar>
-          <v-list-item-title class="headline mb-1">戒めリストのイラスト</v-list-item-title>
-          <v-list-item-subtitle>
-            あなたが今投じた金額は…
-            <br />うな重10杯分です！！
-          </v-list-item-subtitle>
-        </v-list-item-content>
-      </v-list-item>
-    </v-card>
+    <v-row justify="center">
+      <div>あなたが今投じた金額は…</div>
+    </v-row>
+    <v-row justify="center" class=".display-2">
+      <div>{{ judgementResult }}</div>
+    </v-row>
+    <img :src="imagePath" alt="result" />
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      inputedAmount: 0,
+      imagePath: '',
+      judgementResult: ''
+    }
+  },
+  mounted() {
+    this.loadHistories()
+  },
+
+  methods: {
+    // ランダムに画像表示
+    showImage() {
+      const wordsArray = ['うな重', '小説', 'ハーゲンダッツ ']
+      const randomWord =
+        wordsArray[Math.floor(Math.random() * wordsArray.length)]
+
+      if (randomWord === 'うな重' && this.inputedAmount >= 3000) {
+        this.imagePath = '/list_unadon.png'
+        this.judgementResult =
+          'うな重約' +
+          String(Math.floor(this.inputedAmount / 3000)) +
+          '杯分です！！'
+      } else if (randomWord === '小説' && this.inputedAmount >= 500) {
+        this.imagePath = '/list_novel.png'
+        this.judgementResult =
+          '小説約' +
+          String(Math.floor(this.inputedAmount / 500)) +
+          '冊分です！！'
+      } else if (randomWord === 'ハーゲンダッツ' && this.inputedAmount >= 300) {
+        this.imagePath = '/list_haagen-dazs.png'
+        this.judgementResult =
+          'ハーゲンダッツ約' +
+          String(Math.floor(this.inputedAmount / 300)) +
+          '個分です！！'
+      } else {
+        this.imagePath = '/list_umaibou.png'
+        this.judgementResult =
+          'うまい棒約' + String(Math.floor(this.inputedAmount / 10))
+      }
+    },
+    // APIより履歴データをロード
+    async loadHistories() {
+      try {
+        const historyData = await this.$axios.$get(
+          'https://api-server-gtb.herokuapp.com/histories'
+        )
+        // 最新の履歴の課金額取得（直前に入力された課金額）
+        this.inputedAmount = historyData.slice(-1)[0].amounts
+
+        // 課金額に応じて画像を表示
+        this.showImage()
+      } catch (e) {
+        console.error(e)
+      }
+    }
+  }
+}
 </script>
